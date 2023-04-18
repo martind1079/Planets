fe9506eaf82cd06acb351a886801b9bc2042ecd8
Martin Doyle <martindoyle96@gmail.com>
2023-04-18 20:07:26 +0100

    add API End to End tests

--- PlanetsAPIEndToEndTests/PlanetsAPIEndToEndTests.swift
+++ PlanetsAPIEndToEndTests/PlanetsAPIEndToEndTests.swift
@@ -6,13 +6,30 @@
 //
 
 import XCTest
-import Planets
+import CorePlanets
 
 final class PlanetsAPIEndToEndTests: XCTestCase {
 
     func test_httpGETResponse_matchesExpectedResult() {
+        let testServerURL = URL(string: "https://swapi.dev/api/planets/")!
         let client = URLSessionHTTPClient()
-        let loader = RemotePlanetsLoader(client: client)
+        let loader = RemotePlanetsLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "wait for response")
+        var receivedResult: LoadPlanetsResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 20.0)
+        
+        switch receivedResult {
+        case let .success(planets)?:
+            XCTAssertEqual(planets.count, 10, "Expected 10 items in the planets response")
+        case let .failure(error)?:
+            XCTFail("Expeceted success, got \(error) instead")
+        default:
+            XCTFail("Expected success, got no response")
+        }
     }
-
 }
