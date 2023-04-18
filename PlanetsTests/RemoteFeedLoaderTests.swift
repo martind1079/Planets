5b2bfd0e2aeff6854526bacc03561499da23e72c
Martin Doyle <martindoyle96@gmail.com>
2023-04-18 17:33:43 +0100

    Create initial project and test target

---
+++ PlanetsTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,18 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  PlanetsTests
+//
+//  Created by Martin Doyle on 18/04/2023.
+//
+
+import XCTest
+
+protocol Feed {
+    
+}
+
+final class RemoteFeedLoaderTests: XCTestCase {
+
+    
+
+}
